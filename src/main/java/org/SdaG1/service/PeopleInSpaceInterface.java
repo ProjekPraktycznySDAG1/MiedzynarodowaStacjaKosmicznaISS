f6dc1e9c6431f7d0cca88ebd2ff3b2c7eabb430d
zhurakivskyio <zhurakivskyi.o@gmail.com>
2023-02-01 18:52:36 +0100

    feature(iss): added Interfaces DatdServiceInterface and PeopleInSpaceInterface

---
+++ src/main/java/org/SdaG1/service/PeopleInSpaceInterface.java
@@ -0,0 +1,10 @@
+package org.SdaG1.service;
+
+import org.SdaG1.model.People;
+
+import java.util.List;
+
+public interface PeopleInSpaceInterface {
+    public int getAmountOfPeopleInSpace();
+    public List<People> showListOfPeopleInSpace();
+}

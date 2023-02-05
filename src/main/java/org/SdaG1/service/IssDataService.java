f9edf35378dc172c7fb4a3f537e75de4e3b1f6f2
darekszyper <114878453+darekszyper@users.noreply.github.com>
2023-02-05 21:59:09 +0100

    feature(ISS): added getDistanceBetweenTwoPointsInKm(), calculateSpeed(), showCurrentIssLocation() implementations in IssDataService class and modified ConsoleController

--- src/main/java/org/SdaG1/service/IssDataService.java
+++ src/main/java/org/SdaG1/service/IssDataService.java
@@ -2,17 +2,37 @@ package org.SdaG1.service;
 
 import org.SdaG1.model.IssPositionData;
 
-public class IssDataService implements IssDataServiceInterface{
+public class IssDataService implements IssDataServiceInterface {
 
+    JsonSerializerService jsonSerializerService;
 
-    @Override
-    public double calculateSpeed() {
-        return 0;
+    public IssDataService() {
+        this.jsonSerializerService = new JsonSerializerService();
     }
 
     @Override
-    public IssPositionData showCurrentIssLocation() {
-        return null;
+    public double calculateSpeed() throws Exception {
+        IssPositionData issDataPointA = jsonSerializerService.issData().getIss_position();
+        Thread.sleep(5000);
+        IssPositionData issDataPointB = jsonSerializerService.issData().getIss_position();
+        double latA = issDataPointA.getLatitude(), lonA = issDataPointA.getLongitude(),
+                latB = issDataPointB.getLatitude(), lonB = issDataPointB.getLongitude();
+        return getDistanceBetweenTwoPointsInKm(latA, lonA, latB, lonB) / 5 * 3600;
+    }
+
+    private static double getDistanceBetweenTwoPointsInKm(double latA, double lonA, double latB, double lonB) {
+        double r = 6371; // Radius of earth in km
+        double latDiff = Math.toRadians(latB - latA);
+        double lonDiff = Math.toRadians(lonB - lonA);
+        double a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) + Math.cos(Math.toRadians(latA)) *
+                Math.cos(Math.toRadians(latB)) * Math.sin(lonDiff / 2) * Math.sin(lonDiff / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return r * c;
+    }
+
+    @Override
+    public IssPositionData showCurrentIssLocation() throws Exception {
+        return jsonSerializerService.issData().getIss_position();
     }
 
     @Override
